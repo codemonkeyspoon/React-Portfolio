757df8e631c4dd5f5b8a5af9215c725119ba00c0
codemonkeyspoon <98781257+codemonkeyspoon@users.noreply.github.com>
2023-05-28 14:26:08 -0400

    switched to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react';
 import './App.module.scss';
 import BaseLayout from './components/BaseLayout';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
 
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <BaseLayout />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
